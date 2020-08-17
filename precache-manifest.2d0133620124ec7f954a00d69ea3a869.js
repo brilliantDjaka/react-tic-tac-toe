6a82581a765a7a0b2bab834ed95fbb830b6a9798
Brilliant Djaka Irfanuddin Rofiq <brian.rofiq@gmail.com>
2020-08-17 20:42:14 +0700

    Updates

---
+++ precache-manifest.2d0133620124ec7f954a00d69ea3a869.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f13df11cdcdda22bc2fcd4b496e80a1",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "d7a85734c83b06518a44",
+    "url": "/react-tic-tac-toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "185550ba4c511e915492",
+    "url": "/react-tic-tac-toe/static/js/2.e977bf79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.e977bf79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a85734c83b06518a44",
+    "url": "/react-tic-tac-toe/static/js/main.47b8e527.chunk.js"
+  },
+  {
+    "revision": "d9255e6de55285e18dc3",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.81654cbd.js"
+  }
+]);
